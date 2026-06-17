3748cb1b09c72c80f9a8c35857d97cb978a15a5e
Piyumi Sandunika <piyumisandunika2002@gmail.com>
2026-06-17 11:08:22 +0530

    Update App.tsx routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes, Navigate, Outlet } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import DashboardPage from "./features/dashboard/DashboardPage";
 import RoomPage from "./features/room-section/RoomPage";
 import BookingPage from "./features/room-section/BookingPage";
 import OccupancyMatrixPage from "./features/room-section/OccupancyMatrixPage";
 import RemittancePage from "./features/room-section/RemittancePage";
 import RoomDashboardPage from "./features/room-section/RoomDashboardPage";
-import MilkShopDashboard from "./features/milk-shop/MilkShopDashboard";
 import BeerGardenDashboard from "./features/beer-garden/BeerGardenDashboard";
 import Login from "./features/auth/Login";
 
@@ -21,13 +19,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<DashboardPage />} />
           <Route path="rooms/dashboard" element={<RoomDashboardPage />} />
           <Route path="rooms" element={<RoomPage />} />
           <Route path="rooms/bookings" element={<BookingPage />} />
           <Route path="rooms/occupancy" element={<OccupancyMatrixPage />} />
           <Route path="rooms/remittance" element={<RemittancePage />} />
-          <Route path="milk-shop" element={<MilkShopDashboard />} />
         </Route>
 
         {/* Catch-all route to redirect unknown paths to home/login */}
